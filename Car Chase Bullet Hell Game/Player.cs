239a1f1930e41af97c651b75c651bc3c5a32a48e
Logan Kloft <lkloft@outlook.com>
2023-01-24 10:06:51 -0800

    Create basic Player class.

---
+++ Car Chase Bullet Hell Game/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Chase_Bullet_Hell_Game
+{
+    // singleton pattern
+    internal sealed class Player
+    {
+        private static Player _instance;
+        private static readonly object _lock = new object();
+
+        Player() { }
+        public static Player Instance
+        {
+            get
+            {
+                lock( _lock )
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new Player();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
