5cd4fa3537c8c6a8d95efbd3c7ecf81d8da698f0
Logan Kloft <lkloft@outlook.com>
2023-01-24 10:02:38 -0800

    Create basic Shot class.

---
+++ Car Chase Bullet Hell Game/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Chase_Bullet_Hell_Game
+{
+    internal abstract class Shot : Sprite
+    {
+        public float Damage;
+
+        // the behavior for the movement of the bullet
+        public abstract void Move();
+
+        // returns whether the bullet collided with the player
+        public bool Collided()
+        {
+            return base.DestinationRectangle.Intersects(Player.Instance.DestinationRectangle);
+        }
+    }
+}
