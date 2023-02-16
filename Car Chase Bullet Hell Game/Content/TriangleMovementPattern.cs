b49d9beaead0c27bb8676627dd5d62e2541c97f0
Emma Fletcher <emma.l.fletcher@wsu.edu>
2023-02-15 19:22:39 -0800

    Added pauses to the triangular movement pattern

--- Car Chase Bullet Hell Game/Content/TriangleMovementPattern.cs
+++ Car Chase Bullet Hell Game/Content/TriangleMovementPattern.cs
@@ -10,40 +10,52 @@ namespace Car_Chase_Bullet_Hell_Game.Content
 {
     internal class TriangleMovementPattern : MovementPattern
     {
-        private float speed = 2f;
+        private float speed = 5f;
         private float time = 0;
         private Point[] points = { new Point(400, 1000), new Point(1800, 1000), new Point(1000, 500) };
-        int pointIndex = 1;
-        double dx = 0, dy = 0, distance = 0, scale = 0;
+        private int pointIndex = 1;
+        private int instance = 1, lap = 0;
+        private double x_move = 0, y_move = 0, distance = 0, direction = 0;
+        private bool pause = false;
         
         public override void Move(GameTime gameTime, Enemy enemy)
         {
-            if(time==0)
+            if(lap==0)
             {
-                enemy.DestinationRectangle.X = 300;
+                enemy.DestinationRectangle.X = 400;
                 enemy.DestinationRectangle.Y = 1000;
-                time++;
+                lap++;
             }
             if(pointIndex == 3)
             {
                 pointIndex = 0;
             }
-            dx = points[pointIndex].X - enemy.DestinationRectangle.X;
-            dy = points[pointIndex].Y - enemy.DestinationRectangle.Y;
-            distance = Math.Sqrt(dx * dx + dy * dy);
-            scale = 5f / distance;
-            if(dx!=0)
+            if(time%100==0)
             {
-                enemy.DestinationRectangle.X += (int)(dx * scale);
-            }
-            if (dy!=0)
-            {
-                enemy.DestinationRectangle.Y += (int)(dy * scale);
+                pause = false;
+                instance = 1;
+                x_move = points[pointIndex].X - enemy.DestinationRectangle.X;
+                y_move = points[pointIndex].Y - enemy.DestinationRectangle.Y;
+                distance = Math.Sqrt(Math.Pow(x_move, 2) + Math.Pow(y_move, 2));
+                direction = speed / distance;
+                if (x_move != 0)
+                {
+                    enemy.DestinationRectangle.X += (int)(x_move * direction);
+                }
+                if (y_move != 0)
+                {
+                    enemy.DestinationRectangle.Y += (int)(y_move * direction);
+                }
             }
             if(enemy.DestinationRectangle.X == points[pointIndex].X && enemy.DestinationRectangle.Y == points[pointIndex].Y)
             {
+                pause = true;
                 pointIndex++;
             }
-        }
+            if(pause==true)
+            {
+                time++;
+            }
+         }
     }
 }
