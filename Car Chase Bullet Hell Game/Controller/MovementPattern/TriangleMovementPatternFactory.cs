60d559982362caa9fa9664f05a1dfdb005195131
Emma Fletcher <emma.l.fletcher@wsu.edu>
2023-03-25 12:01:43 -0700

    Created factories for right offscreen and triangular movement patterns.

---
+++ Car Chase Bullet Hell Game/Controller/MovementPattern/TriangleMovementPatternFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Chase_Bullet_Hell_Game.Controller.MovementPattern
+{
+    internal class TriangleMovementPatternFactory : MovementFactory
+    {
+        protected override MovementPattern CreateMovementPattern([Optional] Point point, [Optional] int radius)
+        {
+            return new TriangleMovementPattern();
+        }
+    }
+}
